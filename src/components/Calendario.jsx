8907441ee7c8afbc2c9823530b1dd98fda50c516
GGP113 <paulaag316@gmail.com>
2026-02-24 21:35:05 -0500

    Creacion del primer componente y primera pagina de la App. El cambio permite que se renderice el calendario

---
+++ src/components/Calendario.jsx
@@ -0,0 +1,59 @@
+
+import "../styles/StyleCalendarStudents.css";
+
+export function Calendario() {
+  return (
+    <div className="mainCalendar">
+      {/* Header */}
+      <div className="diaCal mesCal">
+        <h2>Feb</h2>
+      </div>
+
+      <div className="diaCal">
+        <h2>23 Lu</h2>
+      </div>
+      <div className="diaCal">
+        <h2>24 Ma</h2>
+      </div>
+      <div className="diaCal">
+        <h2>25 Mi</h2>
+      </div>
+      <div className="diaCal">
+        <h2>26 Ju</h2>
+      </div>
+      <div className="diaCal">
+        <h2>27 Vi</h2>
+      </div>
+
+      {/* Fila Mat 1 */}
+      <div className="matCal">
+        <h2>Mat 1</h2>
+      </div>
+      <div className="cellCal"></div>
+      <div className="cellCal"></div>
+      <div className="cellCal"></div>
+      <div className="cellCal"></div>
+      <div className="cellCal"></div>
+
+      {/* Fila Mat 2 */}
+      <div className="matCal">
+        <h2>Mat 2</h2>
+      </div>
+      <div className="cellCal"></div>
+      <div className="cellCal"></div>
+      <div className="cellCal"></div>
+      <div className="cellCal"></div>
+      <div className="cellCal"></div>
+
+      {/* Fila Mat 3 */}
+      <div className="matCal">
+        <h2>Mat 3</h2>
+      </div>
+      <div className="cellCal"></div>
+      <div className="cellCal"></div>
+      <div className="cellCal"></div>
+      <div className="cellCal"></div>
+      <div className="cellCal"></div>
+    </div>
+  );
+}
---
+++ src/pages/MainView.jsx
@@ -0,0 +1,10 @@
+import { Calendario } from "../components/Calendario";
+import "../styles/MainView.css";
+
+export function MainView() {
+  return (
+    <div className="mainViewContainer">
+      <Calendario />
+    </div>
+  );
+}
